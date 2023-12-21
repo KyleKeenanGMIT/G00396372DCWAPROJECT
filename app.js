88e9e5d9cbd5c0d0e3591f5545b44e1c14d93456
KyleKeenanGMIT <kylekeenan@live.co.uk>
2023-12-21 01:12:01 +0000

    manager data being displayed with _id, name & salary

--- app.js
+++ app.js
@@ -1,20 +1,36 @@
 const express = require('express');
 const app = express();//express install.
+const mongoose = require('mongoose');//mongosee import
 const port = process.env.PORT || 3000; //chosen port
+const Manager = require('./components/managers');//managers.js imported which containts the manager schema.
+
+
 
 app.set('view engine', 'ejs');//setting embedded javascript as my view engine
 
+mongoose.connect('mongodb://localhost/proj2023MongoDB')
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Unaable to connect to MongoDB', err));//connecting to my MONGO DB database i setup for the managers JSON Data.
+
+
 // parsing json bodies
 app.use(express.json());
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.render('home');
 });//renders the home page first when running app.js - home.ejs
 
-app.get('/managers', (req, res) => {
-  // Fetching Manager JSON Data from MONGODB.
-  res.render('managers');
-});
+app.get('/managers', async (req, res) => {
+  try {
+    const managersData = await Manager.find();
+    res.render('managers', { managers: managersData });
+  } catch (error) {
+    console.error('Error fetching managers:', error);
+    res.status(500).send('Error fetching managers data.');
+  }
+});//gather data stored in mongo DB - managers data is _id, name & salary.
 
 app.get('/stores', (req, res) => {
   // will gather stores daya from MYSQL.
---
+++ components/managers.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+// defingin the schema for a managers
+const managerSchema = new mongoose.Schema({
+    _id: String,  
+    name: String,
+    salary: Number
+});
+
+// model created with the collection name
+const Manager = mongoose.model('Manager', managerSchema, 'Managers');
+
+
+
+module.exports = Manager;
