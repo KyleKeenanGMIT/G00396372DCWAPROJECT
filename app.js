94063ec17ac5f3915ec742d154d9b32accab3fe2
KyleKeenanGMIT <kylekeenan@live.co.uk>
2023-12-21 00:33:25 +0000

    app.get for managers.ejs

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ app.get('/', (req, res) => {
   res.render('home');
 });//renders the home page first when running app.js - home.ejs
 
+app.get('/managers', (req, res) => {
+  // Fetching Manager JSON Data from MONGODB.
+  res.render('managers', { managers: yourManagersData });
+});
+
 // Starting the server
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);//logs message saying port is running on port 3000.
